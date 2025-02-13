2e15e99d93827b227ef41b63d30d2684acac1b2f
Elian Garcia <telljua90@gmail.com>
2025-02-13 17:54:02 -0600

    estado para agregar a favoritos

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,8 +1,5 @@
-function Body({ books, search }) {
-  // console.log("Se renderizo el componente Body");
 
-  console.log("valor de search",search);
-  
+function Body({ books, search, handleFavorite }) {
 
   // Filtro el arreglo de libros, busco con el filtro por cada libro su nombre y lo convierto a minuscualas
   // Y tambien convierto a minusculas el nuevo valor de search para comparar y formar un nuevo arreglo
@@ -14,32 +11,34 @@ function Body({ books, search }) {
       {filteredBooks.map((book) => (
         <Card
           key={book.name + book.price}
-          name={book.name}
-          description={book.description}
-          price={book.price}
+          book={book}
+          handleFavorite={handleFavorite}
         ></Card>
       ))}
     </div>
   );
 }
 
-function Card({ name, description, price }) {
-  let precioType = "$" + price;
+function Card({ book, handleFavorite }) {
+  let precioType = "$" + book.price
 
-  // console.log("Se renderizo el componente Card",name);
 
   return (
     <div className={`flex flex-col bg-gray-200 p-4 gap-4 w-48 h-64 rounded-sm`}>
       <div className="flex flex-wrap justify-center items-center h-12 w-full rounded-sm bg-gray-400">
-        <p className="text-sm">{name}</p>
+        <p className="text-sm">{book.name}</p>
       </div>
 
       <div className="relative flex flex-col justify-between h-full">
-        <p className="text-xs">{description}</p>
+        <p className="text-xs">{book.description}</p>
         <span className="self-end text-xs">{precioType}</span>
+        <button onClick={() => handleFavorite(book.name)}
+        className="absolute bottom-0 bg-sky-400">
+          {book.isStarred?"🌟":"⭐"}
+        </button>
       </div>
     </div>
   );
 }
 
-export default Body;
+export default Body
